06241b49b462589fe6d4e889f78424dc4ba0f43f
takayuki murata <takayuki.for.public@gmail.com>
2012-04-24 22:35:32 +0900

    added ex 2-2, exam java add

--- src/main/scala/ch02/Example02.scala
+++ src/main/scala/ch02/Example02.scala
@@ -23,3 +23,27 @@ object Tuns extends App {
     println("low: %s hight: %s".format(low, high))
   }
 }
+
+/**
+ * 2-2 加算の実験
+ *
+ * ２つのリストに入っている数字を加算する。
+ * ２つのリストは同じサイズでなければならない。
+ * もし、異なる桁の数字を加算する場合は０で埋めなければならない。
+ * ex)
+ * 12 + 135 = 147
+ * Add.execute(List(0, 1, 2), List(1, 3, 5) = List(1, 4, 7)
+*/
+object Add extends App {
+  def execute (n1: List[Int], n2: List[Int]): List[Int] = {
+    def calc(x: Int, y: Int): (Int, Int) =
+      if (x + y > 9) {
+        ((x + y) %  10, 1)
+      } else {(x + y, 0)}
+
+    ((n1 zip n2).foldRight((List.empty[Int], 0)) {
+        (elem, sum) => calc(elem._1, elem._2) match {case (value, carry) => ((value + sum._2) :: sum._1, carry)}
+    })._1
+  }
+
+}
--- src/test/scala/ch02/Example02.scala
+++ src/test/scala/ch02/Example02.scala
@@ -10,3 +10,15 @@ class TurnsSpec extends Specification {
   }
 
 }
+
+class AddSpec extends Specification {
+  "add" should {
+    "1 2 + 1 3 5 = 1 4 7" in {
+      Add.execute(List(0, 1, 2), List(1, 3, 5)) == List(1, 4, 7)
+    }
+
+    "carry at 1 digit 8 + 4 = 1 2" in {
+      Add.execute(List(0, 8), List(0, 4))  == List(1, 2)
+    }
+  }
+}
