2bf011bebb8c1fbba8b898a10bd27f4c5846a46b
takayuki murata <takayuki.for.public@gmail.com>
2012-04-24 21:47:03 +0900

    rename object name

--- src/main/scala/ch02/Example02.scala
+++ src/main/scala/ch02/Example02.scala
@@ -4,15 +4,15 @@ package ch02
  * Chapter 2
  *  ex2-1
 */
-object Example02 extends App {
+object Tuns extends App {
 
-  def tuns(target: Int, low: Int, high: Int): Int = {
+  def find(target: Int, low: Int, high: Int): Int = {
     dump(low,high)
     if ((high - low) >= 2) {
       ((low + high) / 2) match {
         case mid if mid == target => 1
-        case mid if mid < target => 1 + tuns(target, mid + 1, high)
-        case mid if mid >= target => 1 + tuns(target, low, mid - 1)
+        case mid if mid < target => 1 + find(target, mid + 1, high)
+        case mid if mid >= target => 1 + find(target, low, mid - 1)
       }
     } else {
      1
--- src/test/scala/ch02/Example02.scala
+++ src/test/scala/ch02/Example02.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable._
 class TurnsSpec extends Specification {
   "turns" should {
     "10 find 1 try range 0 to 10" in {
-      Example02.tuns(5, 0, 10) == 1
+      Tuns.find(5, 0, 10) == 1
     }
   }
 
