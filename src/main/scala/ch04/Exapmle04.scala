b3e5096aabc518fcdf5caafb5daa9361ec536537
takayuki murata <takayuki.for.public@gmail.com>
2012-04-25 12:54:28 +0900

    added compare function

--- src/main/scala/ch04/Exapmle04.scala
+++ src/main/scala/ch04/Exapmle04.scala
@@ -2,13 +2,12 @@ package ch04
 
 object InsertSort {
 
-  // TODO:execute[A <: Ordered[A]
-  def execute[A <: Int](xs: List[A]): List[A] = {
+  def execute[A](xs: List[A])(f: (A, A) => Boolean): List[A] = {
     xs.foldLeft(List.empty[A]) { (sum, elem) =>
       if (sum.isEmpty) {
         elem :: Nil
       } else {
-        val (head, tail) = sum.span{_ < elem }
+        val (head, tail) = sum.span{ f(_, elem) }
         head ::: elem :: tail
       }
     }
--- src/test/scala/ch04/Example04.scala
+++ src/test/scala/ch04/Example04.scala
@@ -5,15 +5,15 @@ import org.specs2.mutable._
 class InsertSortSpec extends Specification {
   "insert" should {
     "1 element list" in {
-      InsertSort.execute(List(1)) == List(1)
+      InsertSort.execute(List(1)){_ < _} == List(1)
     }
 
     " 2 elements list" in {
-      InsertSort.execute(List(2, 1)) == List(1, 2)
+      InsertSort.execute(List(2, 1)){_ < _}  == List(1, 2)
     }
 
     "long lements list " in {
-      InsertSort.execute(List(3, 6, 1, 2, 4, 5)) == List(1, 2, 3, 4, 5, 6)
+      InsertSort.execute(List(3, 6, 1, 2, 4, 5)){_ < _} == List(1, 2, 3, 4, 5, 6)
     }
   }
 }
