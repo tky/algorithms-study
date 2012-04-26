190a663f086f6ae299af2b149949089ffefc9ede
takayuki murata <takayuki.for.public@gmail.com>
2012-04-26 22:37:58 +0900

    added quick sort

--- src/main/scala/ch04/Exapmle04.scala
+++ src/main/scala/ch04/Exapmle04.scala
@@ -13,3 +13,17 @@ object InsertSort {
     }
   }
 }
+
+object QuickSort {
+  def execute[A](xs: List[A])(implicit f: (A, A) => Boolean): List[A] = xs match {
+    case l @ (x :: Nil) => l
+    case (x :: y :: Nil) => if(f(x, y)) { List(x, y) } else { List(y, x) }
+    // クイックソートはどこを基準にとっても同じなので、先頭を基準値として採用。　
+    case (x :: ls) => {
+      val heads = ls.filter(f(_, x))
+      val tails = ls.filterNot(f(_, x))
+      heads ::: x :: execute(tails)
+    }
+    case _ => Nil
+  }
+}
--- src/test/scala/ch04/Example04.scala
+++ src/test/scala/ch04/Example04.scala
@@ -2,7 +2,7 @@ package ch04
 
 import org.specs2.mutable._
 
-class InsertSortSpec extends Specification {
+class SortSpec extends Specification {
   "insert" should {
     "1 element list" in {
       InsertSort.execute(List(1)){_ < _} == List(1)
@@ -14,6 +14,20 @@ class InsertSortSpec extends Specification {
 
     "long lements list " in {
       InsertSort.execute(List(3, 6, 1, 2, 4, 5)){_ < _} == List(1, 2, 3, 4, 5, 6)
+    }
+  }
+
+  "quick" should {
+    "1 element list" in {
+      QuickSort.execute(List(1)){_ < _} == List(1)
+    }
+
+    " 2 elements list" in {
+      QuickSort.execute(List(2, 1)){_ < _}  == List(1, 2)
+    }
+
+    "long lements list " in {
+      QuickSort.execute(List(3, 6, 1, 2, 4, 5)){_ < _} == List(1, 2, 3, 4, 5, 6)
     }
   }
 }
